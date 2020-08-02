236f17b2fd74f8810bc6218fe37f692eefb6fd3e
Tudor <tudordascalu1@gmail.com>
2020-08-02 12:58:46 +0200

    Adds get listener

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const { PORT = 3000 } = process.env;
 
+app.get("/", (req, res) => {
+  res.send("Hello there, have a nice one!");
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
