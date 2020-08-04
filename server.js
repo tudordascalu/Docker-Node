9558b0d61c8725110878473291f37f4e3a5dc121
Tudor <tudordascalu1@gmail.com>
2020-08-04 21:52:53 +0200

    Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.get("/", (req, res) => {
-  res.send("Hello there, have a nice one! How are you doing today?2");
+  res.send("Hello there, have a nice one! How are you doing today?3");
 });
 
 app.listen(PORT, () => {
